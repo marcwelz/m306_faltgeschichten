e3969035926782dd6a83be5966c1f83e5b4615c1
Milchmaa <marc.welz03@gmail.com>
2022-05-10 08:40:36 +0200

    implemented frontend base style

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -4,10 +4,12 @@ import react from 'react';
 export default class Main extends react.Component {
     render() {
         return (
-            <div>
-                <h1>Faltgeschichten</h1>
-                <input type="text" placeholder="Enter gamecode..."></input>
-                <input type="submit"></input>
+            <div className="main">
+                <div className="main-container">
+                    <h1>Faltgeschichten</h1>
+                    <input type="text" placeholder="Enter gamecode..."></input>
+                    <input type="submit"></input>
+                </div>
             </div>
         )
     }
