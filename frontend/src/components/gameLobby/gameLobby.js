03aef638126272201b5921f766d0c69b4475b7e4
Milchmaa <marc.welz03@gmail.com>
2022-05-24 08:32:01 +0200

    added lobby

--- frontend/src/components/gameLobby/gameLobby.js
+++ frontend/src/components/gameLobby/gameLobby.js
@@ -1,12 +1,19 @@
 import './style.css';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import React from "react";
 
 function GameLobby () {
     const {gamecode, username} = useParams();
+    const navigate = useNavigate();
 
     function loadPlayers() {
         
+    }
+
+    function cancelGame() {
+        // TODO cancel game
+
+        navigate("/")
     }
 
     return (
@@ -25,8 +32,17 @@ function GameLobby () {
                     </ul>
                 </div>
                 <div className='main-container__gameoperations'>
-                    <button className="button-9" style={{backgroundColor: '#405cf5'}} value="start"></button>
-                    <button className="button-9"  style={{backgroundColor: "#eb4034"}} value="cancel"></button>
+                    <button 
+                        className="button-9" 
+                        style={{backgroundColor: "#eb4034", marginRight:"10px"}} 
+                        onClick={e => cancelGame()}
+                        value="cancel">
+                    cancel</button>
+                    <button 
+                        className="button-9" 
+                        style={{backgroundColor: '#405cf5', marginLeft: "10px"}} 
+                        value="start">
+                    start</button>
                 </div>
             </div>
         </div>
