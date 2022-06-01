a980386fe413531c9aa29a01210b2c625b892fc2
Raphael Furrer <raphael.furrer@lernende.bbw.ch>
2022-06-01 13:58:43 +0200

    connect frontend backend

--- backend/api/getUsers.php
+++ backend/api/getUsers.php
@@ -2,7 +2,7 @@
 
 require_once "../config/include.inc.php";
 
-global $mysql, $mysql2;
+global $mysql;
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -12,22 +12,28 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-        $lobbyid = htmlentities($_GET['lobbyid'], ENT_QUOTES);
 
-        $stmt = $mysql->prepare("SELECT lobbyID FROM lobby WHERE lobbyID = ?");
-        $stmt->bind_result($lobby);
-        $stmt->bind_param("i", $lobbyid);
-        $stmt->execute();
-        $stmt->fetch();
-        if (isset($lobby)) {
-            http_response_code(400);//Bad Request
-            exit;
+        $lobbyId = htmlentities($_GET["lobbyid"], ENT_QUOTES);
+
+        $statement = $mysql->prepare("SELECT username FROM user WHERE lobbyID = ?;");
+        $statement->bind_result($username);
+        $statement->bind_param("i", $lobbyId);
+        $statement->execute();
+        while( $statement->fetch()){
+            $users[] = $username;
         }
-        $stmt->close();
 
+        if ($users) {
+            http_response_code(200);
+
+            echo json_encode($users);
+        } else {
+            http_response_code(400);
+        }
         break;
     default:
         // 405 = Method Not Allowed
         http_response_code(405); // for PHP >= 5.4.0
         exit;
 }
+
