17e48edcf47468202b3d155a45a60e5a2338632f
Milchmaa <marc.welz03@gmail.com>
2022-06-01 16:40:59 +0200

    made gameform functional

--- frontend/src/components/gamePage/gameForm.js
+++ frontend/src/components/gamePage/gameForm.js
@@ -1,40 +1,65 @@
 import './style.css';
 import { useParams, useNavigate } from 'react-router-dom';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import data from "../../config/data/gameFormText.json";
 
 function GamePage() {
-    const { gamecode, username } = useParams();
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+    const [isDataReady, setDataReady] = useState(false)
     const navigate = useNavigate();
-    const [value, setValue] = useState("");
-    const [question, setQuestion] = useState("Wer?");
-    const [tipp, setTipp] = useState("Denke an eine prominente Person")
+    const [value, setValue] = useState([]);
+    const [question] = useState([]);
+    const [tipp] = useState([])
     const [isFinish, setFinish] = useState(false)
+    const [answers, setAnswers] = useState([])
+
+    const randomTippIndex = Math.floor(Math.random() * (3));
+
+    function handleFinish() {
+        setFinish(!isFinish)
+        if(isFinish) nextQuestion()
+    }
+
+    useEffect(() => {
+        if(!isDataReady) {
+            data.map((data) => {
+                question.push(data.title)
+                tipp.push(data.tipps)
+            })
+        }
+
+        setDataReady(true)
+    })
 
     function nextQuestion() {
-        navigate("")
+        answers[currentQuestion] = value
+        setCurrentQuestion(currentQuestion +1)
+
+        setValue("")
+        if(currentQuestion == 6) {
+            navigate("")
+        }
     }
 
     return (
         <div className="main-game">
             <div className='main-container-game'>
                 <div>
-                    <h1>{question}</h1>
-                    <h3>Tipp: {tipp}</h3>
+                    <h1>{isDataReady ? question[currentQuestion] : "not loaded yet"}</h1>
+                    <h3>Tipp: {isDataReady ? tipp[currentQuestion][randomTippIndex] : "not loaded yet"}</h3>
                 </div>
-
                 <div className="main-container-game-form">
                     <div className='main-container-game-form__input'>
-                        <input type="text" placeholder='Gib deine Antwort ein...' onChange={e => setValue(e.target.value)}></input>
+                        <input type="text" value={value} placeholder='Gib deine Antwort ein...' onChange={e => setValue(e.target.value)}></input>
                     </div>
                 </div>
-
                 <div className='main-container__gameoperations'>
                     <button 
                         className='button-9'
                         style={{ backgroundColor: isFinish ? '#3fcc65' : '#405cf5'}}
-                        onClick={() => setFinish(!isFinish)}
+                        onClick={() => handleFinish()}
                         >
-                            {isFinish ? <div class="lds-ring"><div></div><div></div><div></div><div></div></div>: ""}
+                            {isFinish ? <div className="lds-ring"><div></div><div></div><div></div><div></div></div>: ""}
                             <div style={{width: isFinish ? "auto" : "100%"}}>
                                 {isFinish ? "Wartet auf andere Spieler..." : "Fertig"}
                             </div>
