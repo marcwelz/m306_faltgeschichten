85523010fe26b97aaa2c84f904b75baec3670c65
Milchmaa <marc.welz03@gmail.com>
2022-06-15 10:16:16 +0200

    fixed footer

--- frontend/src/components/static/footer/Footer.js
+++ frontend/src/components/static/footer/Footer.js
@@ -1,16 +1,16 @@
 import './style.css';
-import React from "react";
+import React, { useEffect } from "react";
 import {useParams} from 'react-router-dom';
 
 function Footer() {
     const {gamecode, username} = useParams()
-    
+
     return (
         <footer className='footer-container'>
             {gamecode ? <div className='footer-container__gamecode'>
                 12345678
             </div> : ""}
-            <div className='footer-container__credits'>
+            <div style={gamecode ? {display: "flex", "justify-content": "space-between"}: {}} className='footer-container__credits'>
                 ©2022
             </div>
             {username ? <div className='footer-container__username'>
